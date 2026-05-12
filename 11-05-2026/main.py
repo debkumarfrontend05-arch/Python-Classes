43e811557d24548d6f17a1d7afec68a50255628a
debkumarfrontend05-arch <debkumar.frontend05@gmail.com>
2026-05-12 12:33:36 +0530

    Learning String

---
+++ 11-05-2026/main.py
@@ -0,0 +1,29 @@
+print('Namaeste Youtube We are learning Python Programming');
+
+FirstName = 'Debkumar'; #PascelCase
+firstName = 'Debkumar'; #camelCase
+first_name = 'Debkumar'; #snake_case
+
+
+a = -12;
+b = 15.8;
+c = 14/7;
+v = 34j;
+
+#this all are number data type
+print(type(a)); #integer
+print(type(b)); #float
+print(type(c)); #float
+print(type(v)); #complex
+#this all are number data type
+
+
+#String Data Type
+st = 'Hello how are you';
+print(type(st)); #string
+
+
+#Boolean Data Type
+
+t = True;
+print(type(t)); #boolean
---
+++ 11-05-2026/string.py
@@ -0,0 +1,47 @@
+a = 'D';
+print(ord(a)); #letter to Unicode
+
+
+d = 68;
+print(chr(d)); #Unicode to letter
+
+name = 'Debkumar Mondal';
+print(name[1])
+
+b = "Debkumar Mondal";
+print(b[9::1])
+
+
+#Type Conversion
+
+# c = 12;
+# c = str(c);
+
+# print(type(c));
+
+
+c = '12';
+c = int(c);
+print(type(c));
+
+
+# fasly Values
+# False
+# 0
+# 0.0
+# ""
+# []
+# ()
+# {}
+
+# remaining all are truthy value
+
+
+name = "Debkumar Mondal"
+age = 26;
+
+# print("My name is ",name, "My age is ",age);
+# print(f"my name is {name} and my age is {age}");
+
+newAge = input("Hello what is your age");
+print(f"My age is {newAge}");
