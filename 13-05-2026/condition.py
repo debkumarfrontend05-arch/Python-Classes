6c9ece8b77b242b376dd9137fb0ae8017f951e3a
debkumarfrontend05-arch <debkumar.frontend05@gmail.com>
2026-05-13 12:19:35 +0530

    condition added

--- 13-05-2026/condition.py
+++ 13-05-2026/condition.py
@@ -8,10 +8,32 @@ else:
 
 
 
-money = int(input('Please provide me money mom '));
-if money == 10:
-    print('I will buy mango bar');
-elif money >10:
-    print ('I will buy chocobar');
+# money = int(input('Please provide me money mom '));
+# if money == 10:
+#     print('I will buy mango bar');
+# elif money >10:
+#     print ('I will buy chocobar');
+# else:
+#     print('I will buy a kulfi');
+
+
+
+# If else problem solving
+
+# firstNum = int(input("Enter first Number"));
+# secondNum = int(input("Enter Second Number"));
+
+# if firstNum > secondNum :
+#     print(f"First number is greater {firstNum}");
+# else:
+#     print(f"Second number is greater {secondNum}");
+
+
+gender = str(input("Select a gender "));
+if (gender == 'Male') or (gender == 'male'):
+    print("Good Morning sir");
+elif (gender == 'Female') or (gender == 'female'):
+    print("Good Morning mam");
 else:
-    print('I will buy a kulfi');
+    print('Wrong input');
+
