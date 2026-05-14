0c4d890dbe2ecacded8e65d769507e789bbe2986
debkumarfrontend05-arch <debkumar.frontend05@gmail.com>
2026-05-14 12:54:07 +0530

    Loop started using for

---
+++ 14-05-2026/loop.py
@@ -0,0 +1,45 @@
+# Type of Loops
+
+# for loop 
+# while loop
+
+
+
+# for loop
+
+# range()
+
+# for i in range(1,21,1):
+#     print(i);
+
+
+# for i in range(20,51,1):
+#     print(i);
+
+# for i in range(16,0,-1):
+#     print(i);
+
+# for i in range(-5,-16,-1):
+#     print(i);
+
+# for i in range(1,11,1):
+#     print(f"5 * {i} = {5*i}")
+
+# Loops work in string
+
+
+# name = "Debkumar";
+# for i in range(0,len(name),1):
+#     print(name[i]);
+
+# name  = "My hobby is Coding";
+
+# for i in name:
+#     print(i);
+
+
+for i in range(1,21):
+    if i == 15:
+        continue;
+    else:
+        print(i)
